a93eb0375fdac1ae7f791d4a1f69c1e06ee05985
Darkx23312 <130650239+Darkx-dev@users.noreply.github.com>
2024-07-28 13:54:57 +0530

    Post Not created error fix - 2

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -2,7 +2,7 @@ const debug = require("debug")("blogmos:development");
 const jwt = require("jsonwebtoken");
 
 module.exports = async (req, res, next) => {
-  const token = req.cookies.token
+  const { token } = await req.cookies;
 
   if (!token) {
     debug("TOKEN NOT FOUND");
